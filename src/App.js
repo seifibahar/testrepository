d64e117f5fc277766f3683aefc935d49a321a316
spring.s <seifi.bahar9@gmail.com>
2023-08-22 23:13:18 +0330

    jobfirst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import TopItemes from './TopItemes';
+import TaskItemes from './TaskItemes';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-100 h-100 p-4">
+      <div className="row h-100 justify-content-center align-align-items-start">
+        <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h-fit">
+          <TopItemes />
+          <TaskItemes />
+
+
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
