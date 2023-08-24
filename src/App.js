9bfceba8d61e78a54a8fa6e89eaf126b660d517c
spring.s <seifi.bahar9@gmail.com>
2023-08-24 11:43:08 +0330

    testcommit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,39 @@ import TaskItemes from './TaskItemes';
 
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { MyContext } from './mycontext';
 
 function App() {
+  const [todoItem, settodoItem] = useState([
+    {
+      id: 1,
+      task: "کار شماره 1",
+      active: false,
+
+    },
+    {
+      id: 2,
+      task: "کار شماره 2",
+      active: false,
+    },
+    {
+      id: 3,
+      task: "کار شماره 3",
+      active: true,
+    },
+
+  ]);
+
   return (
     <div className="container w-100 h-100 p-4">
       <div className="row h-100 justify-content-center align-align-items-start">
         <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h-fit">
-          <TopItemes />
-          <TaskItemes />
+          <MyContext.Provider value={{ todoItem, settodoItem }}>
+            <TopItemes />
+            <TaskItemes />
+          </MyContext.Provider>
+
 
 
         </div>
--- src/TaskItemes.jsx
+++ src/TaskItemes.jsx
@@ -1,25 +1,25 @@
-import { Fragment } from "react"
+import { Fragment, useContext } from "react"
+import { MyContext } from "./mycontext";
 
 const TaskItemes = () => {
+  const {todoItem,settodoItem}=useContext(MyContext);
+  
   return (
     <Fragment>
       <ul className="list-group m-0 p-0 mt-2">
-        <li className="list-group-item d-flex justify-content-between">
-          کارشماره 1
-          <span>
-            <i className="me-3 pointer fa fa-check text-success transition_200 text_hover_shadow"></i>
-            <i className="me-3 pointer fa fa-times text-warning transition_200 text_hover_shadow"></i>
-            <i className="me-3 pointer fa fa-trash text-danger transition_200 text_hover_shadow"></i>
-          </span>
-        </li>
-        <li className="list-group-item d-flex justify-content-between">
-          کارشماره 2
-          <span>
-            <i className="me-3 pointer fa fa-check text-success transition_200 text_hover_shadow"></i>
-            <i className="me-3 pointer fa fa-times text-warning transition_200 text_hover_shadow"></i>
-            <i className="me-3 pointer fa fa-trash text-danger transition_200 text_hover_shadow"></i>
-          </span>
-        </li>
+    {
+        todoItem.map(item =>{
+          return(   <li className={`list-group-item d-flex justify-content-between ${item.active ?"list-group-item-success":""}`}>
+          {item.task}
+           <span>
+             <i className="me-3 pointer fa fa-check text-success transition_200 text_hover_shadow"></i>
+             <i className="me-3 pointer fa fa-times text-warning transition_200 text_hover_shadow"></i>
+             <i className="me-3 pointer fa fa-trash text-danger transition_200 text_hover_shadow"></i>
+           </span>
+         </li>)
+       
+        })
+    }
       </ul>
     </Fragment>
   )
